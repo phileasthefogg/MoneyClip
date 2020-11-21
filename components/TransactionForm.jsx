3158363ad0af8619babf9a0e978ba37f93168664
thehlau <thehlau@gmail.com>
2020-11-21 15:20:48 -0800

    Cleaned up Transaction Form Added validation to user input before submission Reformatted the code so that it is more readable, though longer

--- components/TransactionForm.jsx
+++ components/TransactionForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity, Button, Switch, ToastAndroid } from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Toast from './ToastAndroid'
@@ -20,7 +21,6 @@ const generateHash = () => {
 
 
 const Form = (props) => {
-  console.log('FORMKEYS', Object.keys(props))
   const [form, updateForm] = useState({});
   const [datePicker, toggleDatePicker] = useState(false);
   const [amt, updateAmt] = useState(0);
@@ -29,11 +29,29 @@ const Form = (props) => {
   const [visibleToast, setvisibleToast] = useState(false);
 
   useEffect(() => setvisibleToast(false), [visibleToast]);
+
+  const validateRecord = (record) => {
+    let hasDate = record.Date !== undefined;
+    let hasAmount = record.Amount !== undefined;
+    let hasDescription = record.Description !== undefined;
+    let hasTypes = record.Type !== undefined;
+    console.log(
+      hasDate, hasAmount, hasDescription, hasTypes
+    )
+    if (hasDate && hasAmount && hasDescription && hasTypes) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   const createRecord = async (newEntry) => {
-    // console.log(newEntry);
     let newID = generateHash();
-    database.ref(`/users/${props.user.uid}/transactions/`).push(newEntry);
-    // await transactions.add(newEntry);
+    if (validateRecord(newEntry)) {
+      database.ref(`/users/${props.user.uid}/transactions/`).push(newEntry);
+      return new Promise((resolve, reject) => resolve('success'))
+    } else {
+      return new Promise((resolve, reject) => reject('Something went wrong'))
+    }
   }
   const updateType = (string) => {
     let types = string.split(', ');
@@ -55,11 +73,29 @@ const Form = (props) => {
     }
   }
 
+  const renderTags = ({ tag, index, onPress, deleteTagOnPress, readonly }) => (
+    <TouchableOpacity style={styles.tagContainer} key={`${tag}-${index}`} onPress={onPress} >
+      <Text style={{ color: 'white', lineHeight: 12, fontSize: 12 }}>
+        {tag}
+      </Text>
+    </TouchableOpacity>
+  )
+
   return (
     <View style={styles.container}>
+      <StatusBar style="dark" />
       <View style={styles.form}>
-          <TouchableOpacity onPress={() => { toggleDatePicker(true) }} style={{ height: 20, width: '100%', alignItems: 'flex-end' }}>
-            <Text style={{ lineHeight: 20, color: 'grey', fontSize: 15 }} label="Date" name="date" onChangeText={(text) => { updateField('date', text) }}>{(form.Date) ? new Date(form.Date).toDateString() : 'Date'}</Text>
+          <TouchableOpacity
+            onPress={() => { toggleDatePicker(true) }}
+            style={{ height: 20, width: '100%',alignItems: 'flex-end' }}>
+            <Text
+              style={{ lineHeight: 20, color: 'grey', fontSize: 15 }}
+              label="Date"
+              name="date"
+              onChangeText={(text) => { updateField('date', text) }}
+            >
+              {(form.Date) ? new Date(form.Date).toDateString() : 'Date'}
+            </Text>
           </TouchableOpacity>
           {(datePicker) ?
             <DateTimePicker
@@ -94,37 +130,57 @@ const Form = (props) => {
           />
 
 
-        <View style={styles.horizontalRule}>
-        </View>
-          <TextInput style={styles.inputField} label="Description" name="Description" value={form.Description} placeholder="Description" onChangeText={(text) => { updateField('Description', text) }} />
+        <View style={styles.horizontalRule}/>
+        <TextInput
+          style={styles.inputField}
+          label="Description"
+          name="Description"
+          value={form.Description}
+          placeholder="Description"
+          onChangeText={(text) => {
+            updateField('Description', text)
+          }}
+        />
 
-
-        <View style={styles.horizontalRule}>
-        </View>
-
-          <Text style={{ fontSize: 12, color: 'grey' , alignSelf: 'flex-start'}}>Tags:</Text>
-          <Tags
-            textInputProps={{
-              placeholder: "Type"
-            }}
-            onChangeTags={tags => { console.log(tags); let curr = form; curr.Type = tags; updateForm(curr) }}
-            onTagPress={(index, tagLabel, event, deleted) =>
-              console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
-            }
-            containerStyle={{ opacity: 1 }}
-            inputStyle={{ backgroundColor: '#DCDCDC', borderWidth: .5, opacity: 1 }}
-            renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
-              <TouchableOpacity style={styles.tagContainer} key={`${tag}-${index}`} onPress={onPress}>
-                <Text style={{ color: 'white', lineHeight: 12, fontSize: 12 }}>{tag}</Text>
-              </TouchableOpacity>
-            )}
+        <View style={styles.horizontalRule}/>
+        <Text style={{ fontSize: 12, color: 'grey' , alignSelf: 'flex-start'}}>Tags:</Text>
+        <Tags
+          textInputProps={{
+            placeholder: "Type"
+          }}
+          onChangeTags={tags => {
+            let curr = form;
+            curr.Type = tags;
+            updateForm(curr)
+          }}
+          onTagPress={(index, tagLabel, event, deleted) =>
+            console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
+          }
+          containerStyle={{ opacity: 1 }}
+          inputStyle={{
+            backgroundColor: '#DCDCDC',
+            borderWidth: .5,
+            opacity: 1
+          }}
+          renderTag={renderTags}
           />
 
-        <View style={styles.horizontalRule}>
-        </View>
-          <TextInput style={styles.inputField} label="Comment" name="Comment" value={form.Comment} placeholder="Comment" onChangeText={(text) => { updateField('Comment', text) }} />
-
-          <Switch style={{alignSelf:'flex-end'}} onValueChange={(v) => { console.log(v); toggleCash(v); updateField('Cash', v) }} value={cash}></Switch>
+        <View style={styles.horizontalRule}/>
+        <TextInput
+          style={styles.inputField}
+          label="Comment"
+          name="Comment"
+          value={form.Comment}
+          placeholder="Comment"
+          onChangeText={(text) => { updateField('Comment', text) }}
+        />
+        <Switch
+          style={{alignSelf:'flex-end'}}
+          onValueChange={(value) => {
+            toggleCash(value);
+            updateField('Cash', value)
+          }}
+          value={cash}/>
       </View>
 
       <Toast visible={visibleToast} message="Saved Transaction" />
