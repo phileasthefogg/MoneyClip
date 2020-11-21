c73927d9bc95ee2b8b426da01c7ddc784ecabb8d
thehlau <thehlau@gmail.com>
2020-11-21 14:08:40 -0800

    clean up removed unnecessary dependencies and consolidated others if possible

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect, useContext, useMemo } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {firebase} from './firebase/firebase'
 
-
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
 import { createAppContainer, createSwitchNavigator, createStackNavigator, NavigationContainer } from 'react-navigation';
 
 import {AuthenticationContext} from './components/providers/AuthenticationProvider'
--- ContextStack.js
+++ ContextStack.js
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import React, { useContext } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+
 import Home from './components/Home';
 import TransactionList from './components/TransactionList';
-// import Header from './components/Header';
 import Dashboard from './components/Dashboard';
-// import testData from './testData';
 import CustomDrawerContent from './components/NavDrawerComponent'
 
 import { TransactionContext } from './components/providers/TransactionProvider';
@@ -18,50 +14,37 @@ const Drawer = createDrawerNavigator();
 const ContextStack = (props) => {
   const cashContext = useContext(TransactionContext);
   const authContext = useContext(AuthenticationContext)
-    return (
-      <Drawer.Navigator
-        drawerPosition="left"
-        drawerContent={(props) => <CustomDrawerContent {...props} user={authContext.user}/>}
-        drawerContentOptions={{
-          contentContainerStyle: {
-            display: 'flex',
-            // borderColor: 'green',
-            backgroundColor: '#EAE6DA',
-            height: '100%'
-          },
-          // itemStyle: {
-          //   // backgroundColor: 'white'
-          // },
-          activeBackgroundColor: '#D3DEDF'
-        }}
-        drawerStyle={{
-          minWidth: 240,
+  return (
+    <Drawer.Navigator
+      drawerPosition="left"
+      drawerContent={(props) => <CustomDrawerContent {...props} user={authContext.user} />}
+      drawerContentOptions={{
+        contentContainerStyle: {
+          display: 'flex',
+          backgroundColor: '#8A8A8D',
           height: '100%'
-        }}
-      >
-        <Drawer.Screen name="Home" screenOptions={{ drawerLabel: 'Home' }}  >
-          {props => <Home {...props} transactions={cashContext.transactions} />}
-        </Drawer.Screen>
+        },
+        activeBackgroundColor: '#D3DEDF'
+      }}
+      drawerStyle={{
+        minWidth: 240,
+        height: '100%'
+      }}
+    >
+      <Drawer.Screen name="Home" screenOptions={{ drawerLabel: 'Home' }}  >
+        {props => <Home {...props} transactions={cashContext.transactions} />}
+      </Drawer.Screen>
 
-        <Drawer.Screen name="TransactionList" initialParams={{ mini: false, transactions: cashContext.transactions }}>
-          {props => <TransactionList {...props} transactions={cashContext.transactions} />}
-        </Drawer.Screen>
+      <Drawer.Screen name="TransactionList" initialParams={{ mini: false, transactions: cashContext.transactions }}>
+        {props => <TransactionList {...props} transactions={cashContext.transactions} />}
+      </Drawer.Screen>
 
-        <Drawer.Screen name="Dashboard" initialParams={{ mini: false, transactions: cashContext.transactions }}>
-          {props => <Dashboard {...props} transactions={cashContext.transactions} />}
-        </Drawer.Screen>
-      </Drawer.Navigator>
-
-    )
+      <Drawer.Screen name="Dashboard" initialParams={{ mini: false, transactions: cashContext.transactions }}>
+        {props => <Dashboard {...props} transactions={cashContext.transactions} />}
+      </Drawer.Screen>
+    </Drawer.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default ContextStack;
