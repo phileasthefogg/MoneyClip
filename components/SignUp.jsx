d4153247a8c9b3382acd2373dddc898f45d2cc4a
thehlau <thehlau@gmail.com>
2020-11-21 23:54:49 -0800

    refactored to use styled-components also changed font

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,155 +1,180 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { AuthenticationContext } from '../components/providers/AuthenticationProvider';
+import {
+  useFonts,
+  Pacifico_400Regular,
+  AbrilFatface_400Regular
+} from '@expo-google-fonts/dev'
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  width: 100%;
+  flex: 1;
+  alignItems: center;
+  justifyContent: center;
+  display: flex;
+  flexDirection: column;
+  backgroundColor: lightgreen;
+`;
+
+const Header = styled.View`
+  height: 30%;
+  width: 80%;
+  display: flex;
+  justifyContent: center;
+`;
+
+const Title = styled.Text`
+  textAlign: center;
+  fontSize: 80px;
+  color: white;
+`;
+
+const Content = styled.View`
+  height: 70%;
+  width: 100%;
+  backgroundColor: white;
+  alignItems: center;
+  justifyContent: center;
+  display: flex;
+  flexDirection: column;
+  borderWidth: 1px;
+  borderTopRightRadius: 30px;
+  borderTopLeftRadius: 30px;
+`;
+const Form = styled.View`
+  display: flex;
+  flexDirection: column;
+  height: 100%;
+  width: 80%;
+`;
+const InputContainer = styled.View`
+  marginTop: 10%;
+  marginBottom: 10%;
+  height: 50%;
+  display: flex;
+  flexDirection: column;
+  justifyContent: space-evenly;
+`;
+const Field = styled.View`
+  display: flex;
+  flexDirection: column;
+  height: 70px;
+`;
+const Label = styled.Text`
+  fontWeight: bold;
+  fontSize: 15px;
+`;
+const Input = styled.TextInput`
+  borderWidth: .5px;
+  width: 100%;
+  minHeight: 8%;
+  padding: 10px;
+  marginTop: 4px;
+`;
+const ButtonContainer = styled.View`
+  display: flex;
+  height: 20%;
+  width: 100%;
+  flexDirection: column;
+  justifyContent: space-around;
+  alignItems: center;
+`;
+const ButtonView = styled.View`
+  display: flex;
+  alignItems: center;
+  justifyContent: center;
+  width: 80%;
+  height: 40%;
+  color: black;
+  borderWidth: 1px;
+`;
+
 
 const LoginScreen = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+    AbrilFatface_400Regular
+  });
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const AuthContext = useContext(AuthenticationContext);
-  return (
-    <View style={styles.container}>
+  if (fontsLoaded) {
+    return (
+      <Container>
+        <Header>
+          <Title style={{ fontFamily: 'AbrilFatface_400Regular', fontSize: 70 }}>Sign Up</Title>
+        </Header>
+        <Content>
+          <Form>
+            <InputContainer >
+              <Field>
+                <Label>E-mail (will be used as username):</Label>
+                <Input
+                  label="Email"
+                  name="Email"
+                  onChangeText={(text) => {
+                    setEmail(text)
+                  }}
+                  placeholder="Email"
+                />
+              </Field>
+              <Field>
+                <Label>Password (at least 6 characters): </Label>
+                <Input
+                  label="Password"
+                  name="Password"
+                  onChangeText={(text) => {
+                    setPassword(text)
+                  }}
+                  secureTextEntry={true}
+                  placeholder="Password"
+                />
+              </Field>
+              <Field>
+                <Label>Re-Enter Password: </Label>
+                <Input
+                  label="Password"
+                  name="Password"
+                  secureTextEntry={true}
+                  onChangeText={(text) => {
+                    setPassword(text)
+                  }}
+                  placeholder="Password"
+                />
+              </Field>
+            </InputContainer>
+            <ButtonContainer>
+              <ButtonView style={{ backgroundColor: 'lightgreen', color: 'white', borderColor: 'lightgreen' }}>
+                <TouchableOpacity onPress={() => {
+                  AuthContext.signUp(email, password)
+                    .then((user) => { navigation.navigate('HomeScreen') })
+                    .catch((err) => { console.log('couldnt create user: ', err); navigation.navigate('Signup') })
+                }}>
+                  <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>Create User</Text>
+                </TouchableOpacity>
+              </ButtonView>
 
-      <View style={styles.header}>
-        <Text style={styles.title}>Sign Up</Text>
+              <ButtonView style={{ borderColor: 'lightgreen' }}>
+                <TouchableOpacity onPress={() => {
+                  navigation.navigate('Login')
+                }}>
+                  <Text style={{ fontSize: 20, color: 'lightgreen', fontWeight: 'bold' }}>Cancel</Text>
+                </TouchableOpacity>
+              </ButtonView>
+            </ButtonContainer>
+          </Form>
+        </Content>
+      </Container>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator />
       </View>
-      <View style={styles.content}>
-        <View style={styles.form}>
-          <View style={styles.inputs}>
-            <Text style={styles.label}>E-mail (will be used as username):</Text>
-            <TextInput
-              style={styles.inputField}
-              label="Email"
-              name="Email"
-              onChangeText={(text) => {
-                setEmail(text)
-              }}
-              placeholder="Email"
-            />
-            <Text style={styles.label}>Password (at least 6 characters): </Text>
-            <TextInput
-              style={styles.inputField}
-              label="Password"
-              name="Password"
-              onChangeText={(text) => {
-                setPassword(text)
-              }}
-              secureTextEntry={true}
-              placeholder="Password"
-            />
-            <Text style={styles.label}>Re-Enter Password: </Text>
-            <TextInput
-              style={styles.inputField}
-              label="Password"
-              name="Password"
-              secureTextEntry={true}
-              onChangeText={(text) => {
-                setPassword(text)
-              }}
-              placeholder="Password"
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-            <View style={[styles.button, {backgroundColor: 'lightgreen', color:'white', borderColor: 'lightgreen'}]}>
-              <TouchableOpacity onPress={() => {
-                AuthContext.signUp(email, password)
-                  .then((user) => { navigation.navigate('HomeScreen') })
-                  .catch((err) => { console.log('couldnt create user: ', err); navigation.navigate('Signup') })
-              }}>
-                <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Create User</Text>
-              </TouchableOpacity>
-            </View>
-
-            <View style={[styles.button, {borderColor: 'lightgreen'}]}>
-              <TouchableOpacity onPress={() => {
-                navigation.navigate('Login')
-              }}>
-                <Text style={{fontSize: 20, color: 'lightgreen', fontWeight: 'bold'}}>Cancel</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
+    )
+  }
 };
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: 'lightgreen',
-  },
-  header: {
-    // position: 'absolute',
-    // top: 100,
-    height: '30%',
-    width: '80%',
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 80,
-    color: 'white'
-  },
-  content: {
-    height: '70%',
-    width: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    borderWidth: 1,
-    borderTopRightRadius: 30,
-    borderTopLeftRadius: 30,
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    width: '80%',
-  },
-  inputs: {
-    marginTop: '10%',
-    marginBottom: '10%',
-    display: 'flex',
-    flexDirection: 'column',
-    // borderWidth: 1,
-    // padding: 5,
-    height: '50%',
-    justifyContent: 'space-evenly',
-  },
-  inputField: {
-    borderWidth: .5,
-    width: '100%',
-    minHeight: '8%',
-    padding: 10,
-  },
-  label: {
-    fontWeight: 'bold',
-    fontSize: 15,
-  },
-  buttonContainer: {
-    display: 'flex',
-    height: '20%',
-    width: '100%',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  button: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%',
-    height: '40%',
-    color: 'black',
-    borderWidth: 1
-  }
-})
--- pages/LoginScreen.jsx
+++ pages/LoginScreen.jsx
@@ -1,143 +1,168 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { AuthenticationContext } from '../components/providers/AuthenticationProvider';
+import {
+  useFonts,
+  Pacifico_400Regular,
+  AbrilFatface_400Regular
+} from '@expo-google-fonts/dev'
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  width: 100%;
+  flex: 1;
+  alignItems: center;
+  justifyContent: center;
+  display: flex;
+  flexDirection: column;
+  backgroundColor: lightblue;
+`;
+
+const Header = styled.View`
+  height: 30%;
+  width: 80%;
+  display: flex;
+  justifyContent: center;
+`;
+
+const Title = styled.Text`
+  textAlign: center;
+  fontSize: 80px;
+  color: white;
+`;
+
+const Content = styled.View`
+  height: 70%;
+  width: 100%;
+  backgroundColor: white;
+  alignItems: center;
+  justifyContent: center;
+  display: flex;
+  flexDirection: column;
+  borderWidth: 1px;
+  borderTopRightRadius: 30px;
+  borderTopLeftRadius: 30px;
+`;
+const Form = styled.View`
+  display: flex;
+  flexDirection: column;
+  height: 100%;
+  width: 80%;
+`;
+const InputContainer = styled.View`
+  marginTop: 10%;
+  marginBottom: 10%;
+  height: 50%;
+  display: flex;
+  flexDirection: column;
+  justifyContent: space-evenly;
+`;
+const Field = styled.View`
+  display: flex;
+  flexDirection: column;
+  height: 70px;
+`;
+const Label = styled.Text`
+  fontWeight: bold;
+  fontSize: 15px;
+`;
+const Input = styled.TextInput`
+  borderWidth: .5px;
+  width: 100%;
+  minHeight: 8%;
+  padding: 10px;
+  marginTop: 4px;
+`;
+const ButtonContainer = styled.View`
+  display: flex;
+  height: 20%;
+  width: 100%;
+  flexDirection: column;
+  justifyContent: space-around;
+  alignItems: center;
+`;
+const ButtonView = styled.View`
+  display: flex;
+  alignItems: center;
+  justifyContent: center;
+  width: 80%;
+  height: 40%;
+  color: black;
+  borderWidth: 1px;
+`;
+
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+    AbrilFatface_400Regular
+  });
   const AuthContext = useContext(AuthenticationContext);
-  return (
-    <View style={styles.container}>
+  if (fontsLoaded) {
+    return (
+      <Container>
+        <Header>
+          <Title style={{ fontFamily: 'AbrilFatface_400Regular', fontSize: 70 }}>Welcome</Title>
+        </Header>
+        <Content>
+          <Form>
+            <InputContainer>
+              <Field>
+                <Label>E-mail:</Label>
+                <Input
+                  label="Email"
+                  name="Email"
+                  onChangeText={(text) => {
+                    setEmail(text)
+                  }}
+                  placeholder="Email"
+                />
+              </Field>
+              <Field>
+                <Label>Password: </Label>
+                <Input
+                  label="Password"
+                  name="Password"
+                  secureTextEntry={true}
+                  onChangeText={(text) => {
+                    setPassword(text)
+                  }}
+                  placeholder="Password"
+                />
+              </Field>
+            </InputContainer>
+            <ButtonContainer>
+              <ButtonView style={{ backgroundColor: 'lightblue', color: 'white', borderColor: 'lightblue' }}>
+                <TouchableOpacity onPress={() => {
+                  AuthContext.signIn(email, password)
+                    .then((user) => { })
+                    .catch((err) => { console.log('couldnt  sign-in: '); navigation.navigate('Signup') })
+                }}>
+                  <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>Sign In</Text>
+                </TouchableOpacity>
+              </ButtonView>
 
-      <View style={styles.header}>
-        <Text style={styles.title}>Welcome</Text>
+              <ButtonView style={{ borderColor: 'lightblue' }}>
+                <TouchableOpacity onPress={() => {
+                  navigation.navigate('Signup')
+                }}>
+                  <Text style={{ fontSize: 20, color: 'lightblue', fontWeight: 'bold' }}>Sign Up</Text>
+                </TouchableOpacity>
+              </ButtonView>
+            </ButtonContainer>
+          </Form>
+        </Content>
+      </Container>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator />
       </View>
-      <View style={styles.content}>
-        <View style={styles.form}>
-          <View style={styles.inputs}>
-            <Text style={styles.label}>E-mail:</Text>
-            <TextInput
-              style={styles.inputField}
-              label="Email"
-              name="Email"
-              onChangeText={(text) => {
-                setEmail(text)
-              }}
-              placeholder="Email"
-            />
-            <Text style={styles.label}>Password: </Text>
-            <TextInput
-              style={styles.inputField}
-              label="Password"
-              name="Password"
-              secureTextEntry={true}
-              onChangeText={(text) => {
-                setPassword(text)
-              }}
-              placeholder="Password"
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-            <View style={[styles.button, {backgroundColor: 'lightblue', color:'white', borderColor: 'lightblue'}]}>
-              <TouchableOpacity onPress={() => {
-                AuthContext.signIn(email, password)
-                  .then((user) => {  })
-                  .catch((err) => { console.log('couldnt sign-in: '); navigation.navigate('Signup') })
-              }}>
-                <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Sign In</Text>
-              </TouchableOpacity>
-            </View>
-
-            <View style={[styles.button, {borderColor: 'lightblue'}]}>
-              <TouchableOpacity onPress={() => {
-                navigation.navigate('Signup')
-              }}>
-                <Text style={{fontSize: 20, color: 'lightblue', fontWeight: 'bold'}}>Sign Up</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
+    )
+  }
 };
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: 'lightblue',
-  },
-  header: {
-    // position: 'absolute',
-    // top: 100,
-    height: '30%',
-    width: '80%',
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 80,
-    color: 'white'
-  },
-  content: {
-    height: '70%',
-    width: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    borderWidth: 1,
-    borderTopRightRadius: 30,
-    borderTopLeftRadius: 30,
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    width: '80%',
-  },
-  inputs: {
-    marginTop: '10%',
-    marginBottom: '10%',
-    height: '50%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-  },
-  inputField: {
-    borderWidth: .5,
-    width: '100%',
-    minHeight: '8%',
-    padding: 10
-  },
-  label: {
-    fontWeight: 'bold',
-    fontSize: 15,
-  },
-  buttonContainer: {
-    display: 'flex',
-    height: '20%',
-    width: '100%',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  button: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%',
-    height: '40%',
-    color: 'black',
-    borderWidth: 1
-  }
-})
